300d6845a50d26e74914de8b9b8db22074441f6e
chendudai <chendudai10@gmail.com>
2019-05-16 18:27:10 +0300

    starting work on gentree

--- hw3/hw3/gentree.c
+++ hw3/hw3/gentree.c
@@ -9,7 +9,6 @@
 /* definition of the element in the tree */
 typedef struct _ELEMENT* PELEMENT;
 typedef struct _ELEMENT
-{
   pNode obj;
   PELEMENT* children;
   PELEMENT parent;
@@ -19,10 +18,60 @@ typedef struct _ELEMENT
 /* definition of the tree structure */    
 typedef struct _tree{
   PELEMENT head;
-
+  int nodes_num;
+  int k;
+  GetKeyFunction get_key;
+  CloneFunction clone;
+  PrintFunction print_node;
+  DelFunction delete_node;
   /* *** complete the definition of the tree structure *** */
-
 } Tree;
 
 /* *** complete the interface functions implementation *** */
+pTree TreeCreate(GetKeyFunction pGetKeyFunc, CloneFunction pCloneFunc, PrintFunction pPrintFunc, DelFunction pDelteFunc,int k)
+{
+	pTree my_tree = (pTree)malloc(sizeof(Tree));
+	if (my_tree == NULL)
+		return NULL;
+	my_tree->head = NULL;
+	my_tree->nodes_num = 0;
+	my_tree->get_key = pGetKeyFunc;
+	my_tree->clone = pCloneFunc;
+	my_tree->print_node = pPrintFunc;
+	my_tree->delete_node = pDelteFunc;
+	my_tree->k = k;
+	return my_tree;
+}
 
+void TreeDestroy(pTree my_tree)
+{
+	if (my_tree != NULL)
+	//@@@@@@@@@@@@@@@@@@@@@ add free to Nodes
+		free(my_tree);
+}
+
+int TreeNodesCount(pTree my_tree)
+{
+	return my_tree->nodes_num;
+}
+
+void TreePrint(pTree my_tree)
+{
+	pTree tmp = my_tree;
+	if (tmp->head == NULL)
+	{
+		return;
+	}
+	tmp->print_node(my_tree->head->obj);
+	for (int i = 0; i < my_tree->k; i++)
+	{
+		tmp->head = tmp->head->children[i];
+		TreePrint(tmp);
+	}
+}
+
+void TreeAddLeaf(pTree my_tree)
+{
+
+
+}
--- hw3/hw3/gentree.h
+++ hw3/hw3/gentree.h
@@ -4,7 +4,7 @@
 #include "defs.h"
 
 /* complete the definition of the pointer to Tree */
-
+typedef Tree* pTree;
 typedef void* pNode;
 
 typedef int (*GetKeyFunction)(pNode e);
@@ -13,5 +13,8 @@ typedef void (*PrintFunction)(pNode e);
 typedef void (*DelFunction)(pNode e);
 
 /* complete the definition of the interface functions */
+pTree TreeCreate(GetKeyFunction pGetKeyFunc, CloneFunction pCloneFunc, PrintFunction pPrintFunc, DelFunction pDelteFunc, int k);
+void TreeDestroy(pTree my_tree);
+int TreeNodesCount(pTree my_tree);
 
 #endif
